c663f27ae21272c3c5dabd17ecac43c3f214f539
emdas93 <emdas93@gmail.com>
2024-08-15 23:20:07 +0900

    Updates

---
+++ assets/template-CiKDyeHP.js
@@ -0,0 +1,20 @@
+const n=`---\r
+title: "Post List"\r
+description: "Index"\r
+date: "2024-07-08 18:05:00"  # 작성 시간 포함\r
+lastmod: "2024-07-08 18:05:00"  # 마지막 수정 시간\r
+tags: ["index"]\r
+author: "emdas93"\r
+category: "Index"\r
+published: true\r
+slug: "tamplate"\r
+image: ""\r
+draft: false\r
+summary: "Index Page"\r
+permalink: "/post/index"\r
+---\r
+\r
+\r
+# Index Page\r
+\r
+## 처음 만든 페이지입니다....`;export{n as default};
